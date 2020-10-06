c66cb528ff5af66bd4b5fe8c706c85b09e438c5a
johnyu95 <jyu@records.nyc.gov>
2020-10-06 15:36:45 -0400

    Created separate config.rb file

---
+++ common/config/config.rb
@@ -0,0 +1,15 @@
+###############################################################################
+## This file shows the ArchivesSpace configuration values used for DORIS.
+##
+## Note that there is no need to uncomment these unless you plan to change the
+## value from its default.
+###############################################################################
+
+# The following determine which 'tabs' are on the main horizontal menu
+AppConfig[:pui_hide][:repositories] = true
+AppConfig[:pui_hide][:resources] = true
+AppConfig[:pui_hide][:accessions] = true
+
+# Enable / disable PUI resource/archival object page actions
+AppConfig[:pui_page_actions_cite] = false
+AppConfig[:pui_page_actions_request] = false
